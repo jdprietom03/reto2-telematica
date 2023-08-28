8ed2d2412c4dff9a4a06678353e3a1baed40dbf0
Jguerra47 <jguerra47canada@gmail.com>
2023-08-27 20:49:13 -0500

    Add interface runner

--- reto2/src/main.py
+++ reto2/src/main.py
@@ -1,4 +1,25 @@
-from api_gateway import gateway
+import argparse
+import os
+from dotenv import load_dotenv
 
-if __name__ == "__main__":
+# Carga las variables de entorno desde .env
+load_dotenv()
+
+# Configura el parser de argumentos
+parser = argparse.ArgumentParser(
+    description='Ejecuta componentes del proyecto. Puede ser el servidor, el servidor de retry o el gateway.')
+
+parser.add_argument('mode',
+                    type=str,
+                    choices=['server', 'gateway'], 
+                    help='Especifica el componente a ejecutar. "server" para correr el servidor y "gateway" para el gateway.')
+
+args = parser.parse_args()
+
+if args.mode == 'server':
+    # Importa y ejecuta el código del servidor
+    pass
+elif args.mode == 'gateway':
+    # Importa y ejecuta el código del gateway
+    from api_gateway import gateway
     gateway.run()
